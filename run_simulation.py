9f163fdee3ba8461cf57bfe54a22336c189f78e1
BogdaUnT <bogdan34y@proton.me>
2026-01-18 10:57:18 +0100

    v3 (training+plots (raw))

--- run_simulation.py
+++ run_simulation.py
@@ -5,6 +5,9 @@ from common.utils import set_default_params
 import random
 import numpy as np
 
+import matplotlib.pyplot as plt
+from common.utils import show_evaluation, calculate_quality_indicators
+
 """ system = TtsPidSim(tp, pr)
 
 set_default_params(system)
@@ -20,11 +23,11 @@ proc_vars = system.simulate(tend, 'monit', True)
 # PARAMETRY
 # ======================================================
 
-TEND = 800
+TEND = 300
 LOG_MODE = 'monit'
 
-POP_SIZE = 20
-N_GEN = 20
+POP_SIZE = 10
+N_GEN = 10
 MUT_PROB = 0.3
 
 KE_RANGE = (0.2, 5.0)
@@ -49,6 +52,47 @@ def set_defaults_fuzzy(system):
     system.in_var_val('kde', 0.01)
     system.in_var_val('kdCV', 1.0)
 
+def plot_fuzzy_membership():
+    from fuzzylogic.classes import Domain
+    from fuzzylogic.functions import R, S, triangular
+
+    prec = 0.001
+
+    # e
+    e_dom = Domain("e", -1, 1, prec)
+    e_dom.N = S(-1, 1)
+    e_dom.P = R(-1, 1)
+
+    plt.figure()
+    e_dom.N.plot()
+    e_dom.P.plot()
+    plt.title("Zmienna lingwistyczna e")
+    plt.legend(["N", "P"])
+
+    # de
+    de_dom = Domain("de", -1, 1, prec)
+    de_dom.N = S(-1, 1)
+    de_dom.P = R(-1, 1)
+
+    plt.figure()
+    de_dom.N.plot()
+    de_dom.P.plot()
+    plt.title("Zmienna lingwistyczna de")
+    plt.legend(["N", "P"])
+
+    # dCV
+    dcv_dom = Domain("dCV", -1-prec, 1+prec, prec)
+    dcv_dom.N = triangular(-1-prec, -1+prec)
+    dcv_dom.Z = triangular(-prec, prec)
+    dcv_dom.P = triangular(1-prec, 1+prec)
+
+    plt.figure()
+    dcv_dom.N.plot()
+    dcv_dom.Z.plot()
+    dcv_dom.P.plot()
+    plt.title("Zmienna lingwistyczna dCV")
+    plt.legend(["N", "Z", "P"])
+
 
 # ======================================================
 # FUNKCJA CELU
@@ -64,7 +108,7 @@ def cost_function(params):
     system.in_var_val('kde', kde)
     system.in_var_val('kdCV', kdCV)
 
-    data = system.simulate(TEND, 'monit', False)
+    data = system.simulate(TEND, False, False)
 
     e = np.array(data['e'])
 
@@ -182,3 +226,48 @@ if __name__ == "__main__":
     pid_data = run_pid()
     fuzzy_manual = run_fuzzy_manual()
     fuzzy_auto = run_fuzzy_auto(best_params)
+
+    # ===== WYKRESY FUNKCJI PRZYNALEŻNOŚCI =====
+    plot_fuzzy_membership()
+
+    # ===== PID =====
+    eval_pid = calculate_quality_indicators(tp, pid_data['t'], pid_data['SP'][-1], pid_data['e'])
+    show_evaluation(
+        pid_data['t'], pid_data['SP'], pid_data['PV'], pid_data['e'], pid_data['CV'],
+        eval_pid
+    )
+    plt.suptitle("Regulator PID")
+
+    # ===== FUZZY RĘCZNY =====
+    eval_fm = calculate_quality_indicators(tp, fuzzy_manual['t'], fuzzy_manual['SP'][-1], fuzzy_manual['e'])
+    show_evaluation(
+        fuzzy_manual['t'], fuzzy_manual['SP'], fuzzy_manual['PV'],
+        fuzzy_manual['e'], fuzzy_manual['CV'],
+        eval_fm
+    )
+    plt.suptitle("Regulator rozmyty – nastawy ręczne")
+
+    # ===== FUZZY STROJONY =====
+    eval_fa = calculate_quality_indicators(tp, fuzzy_auto['t'], fuzzy_auto['SP'][-1], fuzzy_auto['e'])
+    show_evaluation(
+        fuzzy_auto['t'], fuzzy_auto['SP'], fuzzy_auto['PV'],
+        fuzzy_auto['e'], fuzzy_auto['CV'],
+        eval_fa
+    )
+    plt.suptitle("Regulator rozmyty – strojenie ewolucyjne")
+
+    # ===== WYKRES de(t) =====
+    plt.figure(figsize=(9, 5))
+    plt.plot(fuzzy_auto['t'], fuzzy_auto['de'], label='de')
+
+    kde = best_params[1]
+    plt.plot([0, fuzzy_auto['t'][-1]], [kde, kde], 'y--', label='±kde')
+    plt.plot([0, fuzzy_auto['t'][-1]], [-kde, -kde], 'y--')
+
+    plt.xlabel("t [s]")
+    plt.ylabel("de")
+    plt.title("Pochodna odchyłki – regulator rozmyty strojon y")
+    plt.legend()
+    plt.grid()
+
+    plt.show()
